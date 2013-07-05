f85dde4788faf06cf07c0d4496f0982903d19cd7
Michael Whatley <mike@mike.com>
2013-07-05 16:32:54 -0500

    Initial commit
    this is my class project using sqlite db...using this to keep up with
    class lessons but main project is mysql git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RestaurantSqlite::Application.config.secret_token = '4d06c1160f980472ea4584598415fbf3c5287e849b4e8c301786204bd998d1c03f537b232f7adb2f7c8b04b91d0f4a4670ee5aaaade7545d567a10c195965f9b'
